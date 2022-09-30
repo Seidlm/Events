b2e6a6454a2bf36341897440188702469dd988ab
Michael Seidl | au2mator <michael.seidl@au2mator.com>
2022-09-30 13:57:11 +0200

    ExpertsLiveNL 2022

---
+++ 2022-09-30 - ExpertsLiveNL/2-Groups.ps1
@@ -0,0 +1,86 @@
+#https://learn.microsoft.com/en-us/graph/api/group-post-members?view=graph-rest-1.0&tabs=http
+
+# New Azure AD Group
+# Add User to Group
+
+$ClientID = "your ID"
+$TenantID="your Tenant"
+$ClientSecret="your Secret"
+$MSGRAPHAPI_BaseURL="https://graph.microsoft.com/v1.0"
+
+#Variables
+$GroupName="ExpertsLiveNL2022"
+$GroupDescription="Thats my new Group"
+
+$UserUPN="jasmine.hofmeister@au2mator.com"
+
+
+#Authentication
+#Connect to GRAPH API
+$token_Body = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $clientId
+    Client_Secret = $clientSecret
+}
+$token_Response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $token_Body
+$token_Header = @{
+    "Authorization" = "Bearer $($token_Response.access_token)"
+    "Content-type"  = "application/json"
+}
+#########################################
+
+#New Group
+$NewGroup_Body=@"
+{
+    "description": "$GroupDescription",
+    "displayName": "$GroupName",
+    "groupTypes": [
+        "Unified"
+      ],
+      "mailEnabled": false,
+      "mailNickname": "library",
+      "securityEnabled": true
+    }
+}
+"@
+
+$NewGroup_Params = @{
+    Method = "POST"
+    Uri    = "$MSGRAPHAPI_BaseURL/groups/"
+    header = $token_Header
+    body   = $NewGroup_Body
+}
+
+$NewGroup_Result = Invoke-RestMethod @NewGroup_Params
+
+
+
+
+#Get USer ID
+$GetUser_Params = @{
+    Method = "Get"
+    Uri    = "$MSGRAPHAPI_BaseURL/users/$UserUPN"
+    header = $token_Header
+}
+
+$GetUser_Result = Invoke-RestMethod @GetUser_Params
+
+
+
+#Add User to Group
+$AddUserToGroup_Body=@"
+{
+    "@odata.id": "https://graph.microsoft.com/v1.0/directoryObjects/$($GetUser_Result.id)"
+}
+"@
+
+$AddUserToGroup_Params = @{
+    Method = "POST"
+    Uri    = "$MSGRAPHAPI_BaseURL/groups/$($NewGroup_Result.id)/members/`$ref"
+    header = $token_Header
+    body   = $AddUserToGroup_Body
+}
+
+$AddUserToGroup_Result = Invoke-RestMethod @AddUserToGroup_Params
+
---
+++ 2022-09-30 - ExpertsLiveNL/3-Send Mail.ps1
@@ -0,0 +1,72 @@
+#https://learn.microsoft.com/en-us/graph/api/user-sendmail?view=graph-rest-1.0&tabs=http
+
+# Send Mail via Graph API
+
+$ClientID = "your ID"
+$TenantID="your Tenant"
+$ClientSecret="your Secret"
+$MSGRAPHAPI_BaseURL="https://graph.microsoft.com/v1.0"
+
+#Variables
+$MailSender = "michael.seidl@au2mator.com"
+$Recipient = "michael.seidl@au2mator.com"
+
+
+#Authentication
+#Connect to GRAPH API
+$token_Body = @{
+  Grant_Type    = "client_credentials"
+  Scope         = "https://graph.microsoft.com/.default"
+  Client_Id     = $clientId
+  Client_Secret = $clientSecret
+}
+$token_Response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $token_Body
+$token_Header = @{
+  "Authorization" = "Bearer $($token_Response.access_token)"
+  "Content-type"  = "application/json"
+}
+
+#########################################
+
+
+
+#Send Mail    
+$MailSend_Body = @"
+                    {
+                        "message": {
+                          "subject": "Hello World from Microsoft Graph API",
+                          "body": {
+                            "contentType": "HTML",
+                            "content": "This Mail is sent via Microsoft <br>
+                            GRAPH <br>
+                            API<br>
+                            ExpertsLive NL 2022
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            "
+                          },
+                          "toRecipients": [
+                            {
+                              "emailAddress": {
+                                "address": "$Recipient"
+                              }
+                            }
+                          ]
+                        },
+                        "saveToSentItems": "false"
+                      }
+"@
+
+$MailSend_Params = @{
+  Method = "POST"
+  Uri    = "$MSGRAPHAPI_BaseURL/users/$MailSender/sendMail"
+  header = $token_Header
+  body   = $MailSend_Body
+}
+
+
+
+Invoke-RestMethod @MailSend_Params
