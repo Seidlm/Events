b61b581e73723fe0f896c069ce8de416c0b01b71
Michael Seidl | au2mator <michael.seidl@au2mator.com>
2023-05-23 14:49:20 +0200

    ECS2023

---
+++ 2023-05 - European Collaboration Summit/1-User.ps1
@@ -0,0 +1,59 @@
+#https://learn.microsoft.com/en-us/graph/api/user-update?view=graph-rest-1.0&tabs=http
+
+# Get User Prop
+# Update User Prop
+
+#DON'T DO THIS AT HOME
+$ClientID = "yourClientID"
+$TenantID="yourTenantID"
+$ClientSecret="yourSecret"
+#DON'T DO THIS AT HOME
+
+
+
+
+#Variables
+$UserUPN="jasmine.hofmeister@au2mator.com" #Target User UPN
+$jobTitle="Customer Manager Collaboration Summit Demo" #New Job Title
+$mobilePhone="+43 664 51515115" #New Mobile Phone Number
+
+
+
+#Authentication
+#Connect to GRAPH API
+$token_Body = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $clientId
+    Client_Secret = $clientSecret
+}
+$token_Response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $token_Body
+$token_Header = @{
+    "Authorization" = "Bearer $($token_Response.access_token)"
+    "Content-type"  = "application/json"
+}
+
+
+########################
+#GET
+$GET_URL="https://graph.microsoft.com/v1.0/users/$UserUPN"
+$Response=Invoke-RestMethod -Method GET -Uri $GET_URL -Headers $token_Header
+$Response
+
+
+
+
+
+########################
+#SET
+$Set_body=@"
+{
+    "jobTitle": "$jobTitle",
+    "mobilePhone" : "$mobilePhone"
+}
+"@
+
+$SET_URL="https://graph.microsoft.com/v1.0/users/$UserUPN"
+
+
+Invoke-RestMethod -Method PATCH -Uri $SET_URL -Headers $token_Header -Body $Set_body
---
+++ 2023-05 - European Collaboration Summit/3-Send Mail.ps1
@@ -0,0 +1,76 @@
+#https://learn.microsoft.com/en-us/graph/api/user-sendmail?view=graph-rest-1.0&tabs=http
+
+# Send Mail via Graph API
+
+$ClientID = "yourClientID"
+$TenantID="yourTenantID"
+$ClientSecret="yourSecret"
+$MSGRAPHAPI_BaseURL="https://graph.microsoft.com/v1.0"
+
+#Variables
+$MailSender = "michael.seidl@au2mator.com"
+$Recipient = "michael.seidl@au2mator.com"
+
+
+#Authentication
+#Connect to GRAPH API
+$token_Body = @{
+  Grant_Type    = "client_credentials"
+  Scope         = "https://graph.microsoft.com/.default"
+  Client_Id     = $clientId
+  Client_Secret = $clientSecret
+}
+$token_Response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $token_Body
+$token_Header = @{
+  "Authorization" = "Bearer $($token_Response.access_token)"
+  "Content-type"  = "application/json"
+}
+
+#########################################
+
+
+
+#Send Mail    
+$MailSend_Body = @"
+                    {
+                        "message": {
+                          "subject": "Hello World from Microsoft Graph API",
+                          "body": {
+                            "contentType": "HTML",
+                            "content": "This Mail is sent via Microsoft <br>
+                            <br>
+                            <br>
+                            GRAPH <br>
+                            API<br>
+                            <br>
+                            <br>
+                            European Collaboration Summit 2023
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            "
+                          },
+                          "toRecipients": [
+                            {
+                              "emailAddress": {
+                                "address": "$Recipient"
+                              }
+                            }
+                          ]
+                        },
+                        "saveToSentItems": "false"
+                      }
+"@
+
+$MailSend_Params = @{
+  Method = "POST"
+  Uri    = "$MSGRAPHAPI_BaseURL/users/$MailSender/sendMail"
+  header = $token_Header
+  body   = $MailSend_Body
+}
+
+
+
+Invoke-RestMethod @MailSend_Params
